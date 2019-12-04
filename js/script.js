5fe83cdcb787abe0797e6d8a043e343a0ec68a04
Toni Horne <tonihorne@Tonis-Air.fios-router.home>
2019-12-03 20:53:25 -0500

    API with Images Rendering

--- js/script.js
+++ js/script.js
@@ -3,47 +3,63 @@
 
 //input pieces of data that are constant
 
-const URL_ENDPOINT="https://images-api.nasa.gov/search?q={q}api_key=eS3Ga2uKrrsAaXEbuFmfuewbDskcR1Q6nliWC6Kt"
+const URL_ENDPOINT="https://images-api.nasa.gov/search?q="
 
 
 //the data that can change
 
 
-let input, images;
+let input, images;// items
 
 
 
 /********* Cached Elements **********/
-const $planetsListElement = $('#planets');
-const $inputELement = $('input');
+const $collectionListElement = $('#collection');
+const $inputElement = $('input');
+const $imageListElement = $('#images-list');
 
-document.querySelector('planets');
+//array of elements to store
+//call function to print images in array
+//add image tag to DOM
+
+
 
 /********* Event Listeners **********/
 
-$('#controls').on('click', 'button', handleClick);
+$('#submit-btn').on('click', handleClick);
+$('#more-images-btn').on('click', render);
 
 
 /******** Functions ****************/
 
 
 
-
-
-
-
 function handleClick(){
-  
+     input = $inputElement.val();
+     if(!input) return;
      getAPIData(); 
-     alert($(this).val());  
+
 }
 
 function getAPIData(){
     const response = $.ajax({
-        url: "https://images-api.nasa.gov/search?q={q}api_key=eS3Ga2uKrrsAaXEbuFmfuewbDskcR1Q6nliWC6Kt"
+        url: URL_ENDPOINT + input
     });
     response.then(function(data){
-        console.log(data);
-        console.log(response);
+        images = data.collection.items;
+        render();
     });
-} 
+}
+
+function generateImageUI() {
+    const firstFive = images.splice(0, 5);
+    return firstFive.map(function(element){
+        return `<img src="${element.links[0].href}" alt="${element.data[0].description}">`
+    }).join("");
+}
+
+function render() {
+    if(images.length === 0) return;
+    const imageHtml = generateImageUI();
+    $imageListElement.append(imageHtml);
+}
